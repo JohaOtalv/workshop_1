1b85c14d5d6be1d56d666f8a9085010011a135cf
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2022-12-22 15:41:48 -0500

    solving problems in js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ const API_URL_FEMALE = 'http://localhost:3000/femaleProducts';
 
 let containerCards = document.querySelector('.container-cards');
 
+
+const getData = async (id) => {
+    const respuesta = await fetch(API_URL_MALE + "?id=" + id);
+    const datos = await respuesta.json();
+    let objeto = datos[0]
+    let cont = 0;
+    return objeto;
+  }
+const addToCard = (_id)=>{
+let data = getData(_id)
+let res = localStorage.getItem('cart')
+
+console.log(res);
+if(res== undefined || res ==null){
+let cart =[]
+let cartJson = JSON.stringify(cart)
+localStorage.setItem('cart',cartJson);
+} else{
+    cart = JSON.parse(res)
+}
+cart.push(data);
+let cartJson = JSON.stringify(cart)
+localStorage.setItem('cart',cartJson);
+console.log('add');
+}
+
+
 const loaderCards = async(API)=>{
     const respuesta = await fetch(API)
     const datos = await respuesta.json()
@@ -22,40 +49,22 @@ const loaderCards = async(API)=>{
         <div class="card-body">
         <h2> ${element.name}</h2>
           <h5 class="card-title">$${element.price}</h5>
-          <a href="#" class="btn btn-primary" id="add${element.id}" >Add</a>
+          <a class="btn btn-primary" id="add${element.id}" >Add</a>
         `
         containerCards.appendChild(div);
         // funciones
         const button = document.getElementById('add'+ element.id);
-        button.addEventListener('click',()=>{
+        button.addEventListener('click',()=>{ 
+            console.log('me undiste');
             addToCard(element.id);
+            
         })
         });
-        
+    
     } catch (error) {
         
     }
 }
+
 loaderCards(API_URL_MALE);
 loaderCards(API_URL_FEMALE);
-
-const getData = async (id) => {
-    const respuesta = await fetch(API_URL_MALE + "?id=" + id);
-    const datos = await respuesta.json();
-    let objeto = datos[0]
-    let cont = 0;
-    return objeto;
-  }
-const addToCard = (_id)=>{
-let data = getData(_id)
-let res = localStorage.getItem('cart')
-if(res== undefined || res ==null){
-let cart =[]
-} else{
-    cart = JSON.parse(res)
-}
-cart.push(data);
-let cartJson = JSON.stringify(cart)
-localStorage.setItem('cart',cartJson);
-console.log('add');
-}
