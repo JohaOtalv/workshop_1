ecfc359fd533dfbd0d8dc7b77177cec8a77435fe
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2022-12-22 17:38:38 -0500

    add cart

--- src/index.js
+++ src/index.js
@@ -1,70 +1,69 @@
 import './styles/style.css'
-import  logo from './img/logo.png';
+import logo from './img/logo.png';
 const API_URL = 'http://localhost:3000/Products';
 
 
 let containerCards = document.querySelector('.container-cards');
 
 
-const getData = async (id) => {
-    const respuesta = await fetch(API_URL + "?id=" + id);
-    const datos = await respuesta.json();
-    let objeto = datos[0]
-    let cont = 0;
-    return objeto;
-  }
-const addToCard = (_id)=>{
-let data = getData(_id)
-let res = localStorage.getItem('cart')
+// const getData = async (id) => {
+//     const respuesta = await fetch("http://localhost:3000/Products?id=" + id);
+//     const datos = await respuesta.json();
+//     let objeto = datos[0]
+//     return objeto;
+//   }
+let cart; 
+const addToCard = (_object) => {
+    let res = localStorage.getItem('cart')
 
-console.log(res);
-if(res== undefined || res ==null){
-let cart =[]
-let cartJson = JSON.stringify(cart)
-localStorage.setItem('cart',cartJson);
-} else{
-    cart = JSON.parse(res)
+    console.log(res);
+    if (res == undefined || res == null) {
+         cart = []
+        let cartJson = JSON.stringify(cart)
+        localStorage.setItem('cart', cartJson);
+    } else {
+       let cart = JSON.parse(res)
+    }
+    cart.push(_object);
+    let cartJson = JSON.stringify(cart)
+    localStorage.setItem('cart', cartJson);
+    console.log('add');
 }
-cart.push(data);
-let cartJson = JSON.stringify(cart)
-localStorage.setItem('cart',cartJson);
-console.log('add');
-}
 
 
-const loaderCards = async(API)=>{
+const loaderCards = async (API) => {
     const respuesta = await fetch(API)
     const datos = await respuesta.json()
     console.log(datos);
-    
+
     try {
         containerCards.innerHTML += ``
         datos.forEach(element => {
-            
-            
-        const div = document.createElement('div');
-        div.setAttribute('class', 'card cartas');
-        div.innerHTML =`
+
+
+            const div = document.createElement('div');
+            div.setAttribute('class', 'card cartas');
+            div.innerHTML = `
         <img src="${element.image}" class="card-img-top" alt="...">
         <div class="card-body">
         <h2> ${element.name}</h2>
           <h5 class="card-title">$${element.price}</h5>
           <a class="btn btn-primary" id="add${element.id}" >Add</a>
         `
-        containerCards.appendChild(div);
-        // funciones
-        const button = document.getElementById('add'+ element.id);
-        button.addEventListener('click',()=>{ 
-            console.log('me undiste');
-            addToCard(element.id);
-            
-        })
+            containerCards.appendChild(div);
+            // funciones
+            const button = document.getElementById('add' + element.id);
+            button.addEventListener('click', () => {
+                console.log('me undiste');
+                addToCard(element);
+
+            })
         });
-    
+
     } catch (error) {
-        
+
     }
-   
+
 }
 
 loaderCards(API_URL);
